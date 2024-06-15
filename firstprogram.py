a42c711c19746d9859b8802f2a8142b437c67011
StarloTheSheriff <luminanceswebsite@mail.ru>
2024-06-16 00:45:47 +0300

    Create firstprogram.py

---
+++ firstprogram.py
@@ -0,0 +1,4 @@
+print("Howdy, partner! My name's Starlo, but you can call me North Star, the fearless sheriff of the Wild East.")
+print("I'm a humanoid monster with yellow skin and star-like appendages on my head, sportin' teal patterns.")
+print("I wear a brown poncho with teal stripes, a dark brown ten-gallon hat, brown pants, and black boots with spurs.")
+print("And don't even get me started on my trusty silver revolver!")
